80979ce1f95b927659f059d7a46fd09dd71d893d
Idil Sincer <idilo.sincero@gmail.com>
2026-06-04 15:44:19 +0300

    ft_atoi

---
+++ ft_atoi.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: idilsincer <idilsincer@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/04 15:08:12 by idilsincer        #+#    #+#             */
+/*   Updated: 2026/06/04 15:43:23 by idilsincer       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(const char *nptr)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (nptr[i] == ' ' && nptr[i] >= 9 && nptr[i] <= 13)
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign -= 1;
+		i++;
+	}
+	while (nptr[i] >= '0' && '9')
+	{
+		result = result * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
